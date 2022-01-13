5d9f0bd38ee8ed27f9803cdaf41ac74430397696
syntaxnd <91348804+syntaxnd@users.noreply.github.com>
2022-01-13 14:53:13 -0600

    Added google analytics tracking ID

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,7 +57,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `G-JKMNLT8KBQ`,
       },
     },
     {
