54e48ebfc895bbe7e26be5ce4779f60db358ade6
Jason Ross <jaross@microsoft.com>
2022-01-22 00:18:19 -0600

    Windows Terminal Transparency Post

--- src/pages/uses.js
+++ src/pages/uses.js
@@ -1,22 +1,26 @@
-import React from 'react';
+import React from "react";
 
 export default function UsesPage() {
-    return (
-      <>
+  return (
+    <>
       <div className="item1">
         <h1>Uses</h1>
-        <p>I often get asked what type of hardware or software I use and I keep track of it all on this page. I change this up often so check back for regular updates.</p>
+        <p>
+          I often get asked what type of hardware or software I use and I keep
+          track of it all on this page. I change this up often so check back for
+          regular updates.
+        </p>
         <h2>Hardware</h2>
         <ul>
-            <li>Laptop: MacBook Pro 2018</li>
-            <li>Laptop 2: Surface Book 2</li>
-            <li>Mouse: Logitech MX Master</li>
-            <li>Keyboard: Logitech MX Keys</li>
+          <li>Laptop: MacBook Pro 2018</li>
+          <li>Laptop 2: Surface Book 2</li>
+          <li>Mouse: Logitech MX Master</li>
+          <li>Keyboard: Logitech MX Keys</li>
         </ul>
         <h2>Software</h2>
         <ul>
-            <li>Terminal: HyperJS (on Mac OS) and Windows Terminal</li>
-            <li>Code Editor: VSCode, NeoVim</li>
+          <li>Terminal: HyperJS (on Mac OS) and Windows Terminal</li>
+          <li>Code Editor: VSCode, NeoVim</li>
         </ul>
         <h2>VSCode Extensions</h2>
         <ul>
@@ -31,6 +35,6 @@ export default function UsesPage() {
           <li>Markdown Preview Enhanced</li>
         </ul>
       </div>
-      </>
-      )
-    }
+    </>
+  );
+}
