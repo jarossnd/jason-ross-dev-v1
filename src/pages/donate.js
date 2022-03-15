e6d6b0a69bfac5ae023b0394ea0d80ede4d27fa0
Jason Ross <jason@jasonross.xyz>
2022-03-15 07:52:41 -0500

    working on adding donation links

--- src/pages/donate.js
+++ src/pages/donate.js
@@ -1,5 +1,32 @@
 import React from 'react';
 import SEO from '../components/SEO';
+import styled from 'styled-components';
+
+
+const DonateStyles = styled.div`
+  .donate-container {
+    border: 3px solid var(--black);
+    border-radius: 15px;
+    font-size: 3rem;
+    text-decoration: none;
+    margin-bottom: 20px;
+    padding: 20px;
+    background-color: var(--blue);
+  }
+
+  @media screen and (max-width: 760px) {
+    .donate-container {
+      border: 3px solid var(--black);
+      border-radius: 15px;
+      font-size: 2rem;
+      text-decoration: none;
+      margin-bottom: 10px;
+      padding: 10px;
+      background-color: var(--blue);
+    }
+  }
+`;
+
 
 export default function DonatePage() {
   return (
@@ -18,6 +45,12 @@ export default function DonatePage() {
           </a>
         </p>
       </div>
+	  <DonateStyles>
+	  <div className="donate-container">
+	  <h2>Monero</h2>
+	  <p>XMR: 8BdJhzfMn1BfwF8gNxFoS4DBv3HtwNzGBNAmLFaJHmcv6Tt7vuwVb8Y9z7v9oogFskFx4wPWiUcQw9CmmHURzwi3JP6xDLG</p>
+	  </div>
+	  </DonateStyles>
     </>
   );
 }
