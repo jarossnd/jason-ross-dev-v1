f0114e567cda46c6dcb9dd02af1033eaf60f1d7a
Jason Ross <jason@jasonross.xyz>
2022-04-04 22:19:01 -0500

    working on blog post

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,30 +8,38 @@ export default function AboutPage() {
       <SEO title="About" />
       <div className="item1">
         <h1>About</h1>
-        <h2>Howdy, my name is Jason! I'm glad you are here 💩</h2>
+        <h2>Howdy, my name is Jason 😊</h2>
         <p>
-          I currently work for a fortune 500 company helping people get things
-          done.
+          I live in the United States in an area where summers are hot, winter
+          are cold, and is windy all the time.
         </p>
         <p>
-          When I’m not working, I enjoy web development, exploring new software,
-          and using Linux based operating systems. I also create technology
-          videos on my YouTube channel found{' '}
+          I enjoy anything that has to do with Linux based operating systems,
+          coding, web servers, and everything in between. Some of my favorite
+          utilities are VS Code, NeoVim, and Git. Occasionally, I also create
+          technology videos on my YouTube channel found{' '}
           <a href="https://www.youtube.com/channel/UCP6Y5xvu8VSyXjFHwGMgc6g">
             here
           </a>
           .
         </p>
         <p>
-          You will find me using both Windows, MacOS, and Linux operating
-          systems. The company I work for supports all 3 platforms, so I need to
-          stay on top of it. Besides where I work, I also believe that you
-          should know the ins and outs of each major OS to help your clients.
+          I started my own computer repair business when I was a young teenager
+          making house calls and fixing computers for local businesses. I used
+          to advertise my business with advertisements glued to the tops of
+          pizza boxes at a local pizza restaurant. A few years later I was
+          developing and designing websites while hosting the sites on my own
+          servers that I managed.
         </p>
         <p>
-          Sometimes I get questions on what hardware and software I use. You can
-          view my stuff on my <Link to="/uses">uses</Link> page.
+          Nowadays I work for a large software company helping people and
+          customers get things done.
         </p>
+        <p>
+          Sometimes I get questions about what hardware and software I use. You
+          can view the stuff I use on my <Link to="/uses">uses</Link> page.
+        </p>
+        <p>Thanks for visiting! 🍻</p>
       </div>
     </>
   );
