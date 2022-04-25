8bb3a094f533be9be293b0e6f323b6524a93e365
Jason Ross <jaross@microsoft.com>
2022-04-25 15:09:56 -0500

    Removed site map

--- gatsby-config.js
+++ gatsby-config.js
@@ -73,50 +73,6 @@ module.exports = {
       },
     },
     {
-      resolve: 'gatsby-plugin-sitemap',
-      options: {
-        query: `
-      {
-        allSitePage {
-          nodes {
-            path
-          }
-        }
-        allWpContentNode(filter: {nodeType: {in: ["Post", "Page"]}}) {
-          nodes {
-            ... on WpPost {
-              uri
-              modifiedGmt
-            }
-            ... on WpPage {
-              uri
-              modifiedGmt
-            }
-          }
-        }
-      }
-    `,
-        resolveSiteUrl: () => siteUrl,
-        resolvePages: ({
-          allSitePage: { nodes: allPages },
-          allWpContentNode: { nodes: allWpNodes },
-        }) => {
-          const wpNodeMap = allWpNodes.reduce((acc, node) => {
-            const { uri } = node;
-            acc[uri] = node;
-
-            return acc;
-          }, {});
-
-          return allPages.map((page) => ({ ...page, ...wpNodeMap[page.path] }));
-        },
-        serialize: ({ path, modifiedGmt }) => ({
-          url: path,
-          lastmod: modifiedGmt,
-        }),
-      },
-    },
-    {
       resolve: 'gatsby-plugin-robots-txt',
       options: {
         host: 'https://www.jasonross.dev',
